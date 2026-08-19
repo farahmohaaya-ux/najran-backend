4b07eaf8b6f22238bc233d98d83f3ad94213cfdb
farahmohaaya-ux <farahmohaaya@gmail.com>
2026-08-19 18:33:16 +0300

    Update clients.js

--- clients.js
+++ clients.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const pool = require('../db/db');
-const { requireAuth, requireModule } = require('./auth');
+const { requireAuth, requireModule } = require('../middleware/auth');
 
 const router = express.Router();
 
