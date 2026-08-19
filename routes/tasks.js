be00d712e18c26c5c1964fb98a758472402c3bc7
farahmohaaya-ux <farahmohaaya@gmail.com>
2026-08-19 17:54:35 +0300

    Update tasks.js

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const pool = require('../db/db');
-const { requireAuth } = require('../middleware/auth');
+const { requireAuth } = requirerequire('./auth');
 
 const router = express.Router();
 const VALID_STATUSES = ['today', 'progress', 'review', 'revision', 'done'];
