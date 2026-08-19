5e358610c4ce2d447eb87b4d22123a6e360a0f65
farahmohaaya-ux <farahmohaaya@gmail.com>
2026-08-19 17:50:24 +0300

    Update finance.js

--- finance.js
+++ finance.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const pool = require('../db/db');
-const { requireAuth, requireModule } = require('../middleware/auth');
+const { requireAuth, requireModule } = requirerequire('./auth');
 
 const router = express.Router();
 
